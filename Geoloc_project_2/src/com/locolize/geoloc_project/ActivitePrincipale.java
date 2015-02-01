cb0f11cc6be7acf926d9d2695107b0da3703b46b
Robiniouze <robincahierre@gmail.com>
2015-02-01 15:04:47 +0100

    Update ActivitePrincipale.java

--- Geoloc_project_2/src/com/locolize/geoloc_project/ActivitePrincipale.java
+++ Geoloc_project_2/src/com/locolize/geoloc_project/ActivitePrincipale.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.content.Intent;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,6 +24,7 @@ import android.content.Context;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -64,9 +66,7 @@ public class ActivitePrincipale extends Activity implements LocationListener{
 	private MarkerOptions[] places;
 	private MarkerOptions options = new MarkerOptions();
 	Marker marqueur_rdv_courant;
-	
-	
-	//Utilisateur user = new Utilisateur();
+	private boolean addMarker = false;
 	
 	
 	 //static final LatLng HAMBURG = new LatLng(53.558, 9.927);
@@ -85,6 +85,33 @@ public void onCreate(Bundle savedInstanceState) {
 		otherIcon = R.drawable.purple_point;
 		friendIcon = R.drawable.purple_point;
 		
+		ArrayList<Contact> contacts_tab = new ArrayList<Contact>(0);
+		Utilisateur user = new Utilisateur();
+		double lat1 = 48.853;
+		double lon1 = 2.35; //Notre Dame de Paris
+		double lat2 = 48.855159;
+		double lon2 = 2.361385; //Carrousel du Louvre
+		double lat3 = 48.8583700999;
+		double lon3 = 2.2944813000; //Tour Eiffel
+		
+		Contact c1 = new Contact(); 
+		//Contact c2 = new Contact();
+		//user.close_contacts.add(c2);
+		LatLng LaLo = new LatLng(lat1,lon1);
+		//c1.setLatLng(LaLo);
+		//c1.position.setLatitude(lat1);
+		//c1.position.setLongitude(lon1);
+		c1.latlng = new LatLng(lat1,lon1);
+		c1.pseudo="ND de Paris"; //c1.name="a";c1.surname="a";c1.position= new Location("Paris");c1.id=1;c1.phone_number=0;
+		c1.visible=true;
+		//c2.position.setLatitude(lat2); c2.position.setLongitude(lon2); c2.pseudo="Carrousel du Louvre";
+		//Contact c3 = new Contact(); c3.position.setLatitude(lat3); c3.position.setLongitude(lon3); c3.pseudo="Tour Eiffel";
+		//user.name = "alpha";
+		user.close_contacts = contacts_tab;
+		user.close_contacts.add(c1);
+		//user.close_contacts.add(c2);
+		//user.close_contacts.add(c3);
+		
 		if(theMap==null){
 			//on "recupere" la carte
 			theMap = ((MapFragment)getFragmentManager().findFragmentById(R.id.map)).getMap();
@@ -97,6 +124,7 @@ public void onCreate(Bundle savedInstanceState) {
 					@Override
 					public void onMapLongClick(LatLng point)
 					{
+						if(addMarker == true){
 						if(options==null)
 						{
 							options = new MarkerOptions()
@@ -115,8 +143,14 @@ public void onCreate(Bundle savedInstanceState) {
 							marqueur_rdv_courant = theMap.addMarker(options);
 						}
 						//new GetTask().execute(point);
+						addMarker = false;
+						//lancer activité partager à : avec des checkboxes
+						//
+						}
+						
 					}	
 				});
+				
 				/*
 				theMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
 					
@@ -124,10 +158,11 @@ public void onCreate(Bundle savedInstanceState) {
 					public boolean onMarkerClick(Marker arg0) {
 						// TODO Auto-generated method stub
 						//On affiche une bulle dans la même fenetre qui contient un ou plusieurs boutons
+						
 						return false;
 					}
-				});
-				*/
+				})*/;
+				
 				
 				
 				
@@ -135,7 +170,7 @@ public void onCreate(Bundle savedInstanceState) {
 				  
 				
 
-				//printContacts(user);
+				user.printContacts(theMap);
 				
 				//mise a jour de la position de l'utilisateur et des points d'interets obtenus
 				//par les requetes places API
@@ -145,7 +180,7 @@ public void onCreate(Bundle savedInstanceState) {
 		
 	    final Button contactsButton = (Button) findViewById(R.id.Contacts);
 	    final Button optionsButton = (Button) findViewById(R.id.Options);
-	    
+	    final Button addMarkerButton = (Button) findViewById(R.id.AddMarkerRDV);
 	    
 	    contactsButton.setOnClickListener(new OnClickListener() {
 	    	  @Override
@@ -168,6 +203,18 @@ public void onCreate(Bundle savedInstanceState) {
 	    		  
 	    		}
 	    	});
+	    
+	    addMarkerButton.setOnClickListener(new OnClickListener(){
+	    	@Override
+	    	public void onClick(View v){
+	    		//Mettre à un l'ajout de marqueur;
+			    addMarker=true;
+	    		
+	    		//Afficher un toast
+	    		Toast.makeText(getApplicationContext(), 
+                         "Veuillez cliquer sur la carte pour ajouter le marqueur", Toast.LENGTH_LONG).show();
+	    	}
+	    });
 	   
 	}
 
@@ -188,13 +235,7 @@ public void onProviderEnabled(String provider) {
 public void onStatusChanged(String provider, int status, Bundle extras) {
 	Log.v("ActivitePrincipale", "status changed");
 }
-/*
-public void printContacts(Utilisateur user){
-	for(Contact contact : user.close_contacts)
-	{
-		theMap.addMarker(contact.createMarker());
-	}
-}*/
+
 
 private void updatePlaces(){
 	//on obtient le gestionnaire de lieu "location manager"
