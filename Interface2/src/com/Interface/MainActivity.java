5566fbbc419f5b2856c9254c03f7065ce5b75f1f
huguestav <hugues.tavenard@eleves.enpc.fr>
2015-02-06 02:26:54 +0100

    On peut envoyer des sms et les recevoir dans l'aplli

--- Interface2/src/com/Interface/MainActivity.java
+++ Interface2/src/com/Interface/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends Activity {
 	    final Button contactsButton = (Button) findViewById(R.id.Contacts);
 	    final Button optionsButton = (Button) findViewById(R.id.Options);
 	    final Button CreerCompteButton = (Button) findViewById(R.id.CreerCompte);
+	    final Button SMSButton = (Button) findViewById(R.id.SMSactivity);
 	    
 	    contactsButton.setOnClickListener(new OnClickListener() {
 	    	  @Override
@@ -47,6 +48,15 @@ public class MainActivity extends Activity {
 	    	  public void onClick(View v) {
 		  		Intent intent3 = new Intent(MainActivity.this, CreerCompteActivity.class);
 		  		startActivity(intent3);
+	    		  
+	    		}
+	    	});
+	    
+	    SMSButton.setOnClickListener(new OnClickListener() {
+	    	  @Override
+	    	  public void onClick(View v) {
+		  		Intent intent = new Intent(MainActivity.this, SMS.class);
+		  		startActivity(intent);
 	    		  
 	    		}
 	    	});
